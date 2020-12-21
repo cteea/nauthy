db5af65f3226d545995edb9b73a3b5290ead0eff
lzoz <_foo@tuta.io>
2020-12-22 03:52:52 +1100

    add test cases for totp

--- tests/test_otp.nim
+++ tests/test_otp.nim
@@ -1,5 +1,5 @@
 import sequtils, sugar, strutils
-import "../otp", "../common"
+import "../otp", "../common", "../hmac"
 
 proc testHotp() =
     ## Test HOTP implementation using the test values provided in RFC4226.
@@ -12,4 +12,14 @@ proc testHotp() =
         let value = hotp(secret, counter)
         doAssert value == correct, "Test for otp.hotp() failed; result = $1, correct_value = $2" % [$value, $correct]
 
+proc testTotp() =
+    ## Test TOTP implementation using the test values provided in RFC6238.
+    let secret: Bytes = "12345678901234567890".map(c => (byte)(c))
+    let correctValues = [(59'i64, "94287082"), (1111111109'i64, "07081804"), (1111111111'i64, "14050471"),
+                         (1234567890'i64, "89005924"), (2000000000'i64, "69279037"), (20000000000'i64, "65353130")]
+    for (time, correct) in correctValues:
+        let value = totp(secret, 8, 30, hmacSha1, time)
+        doAssert value == correct, "Test for otp.totp() failed; result = $1, correct_value = $2" % [$value, $correct]
+
 testHotp()
+testTotp()
