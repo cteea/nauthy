1cd1d54bd28b2704d778b60340fd42797de772ea
lzoz <_foo@tuta.io>
2020-12-22 18:23:16 +1100

    add hotp.at() and totp.at() procs

--- otp.nim
+++ otp.nim
@@ -28,7 +28,7 @@ proc newTotp*(key: Bytes, length: OtpValueLen = 6, interval: TimeInterval = 30,
               hashFunc: HashFunc = sha1Hash, t0: EpochSecond = 0,): Totp =
     result = (key, length, interval, hashFunc, t0)
 
-proc hotp*(key: Bytes, counter: uint64, digits = 6, hash: HashFunc = sha1Hash): string =
+proc hotp*(key: Bytes, counter: BiggestUInt, digits: OtpValueLen = 6, hash: HashFunc = sha1Hash): string =
     ## Generates HOTP value from `key` and `counter`.
     let c: Bytes = intToBytes(counter)
     let mac: Bytes = hmacX(key, c, hash)
@@ -37,14 +37,21 @@ proc hotp*(key: Bytes, counter: uint64, digits = 6, hash: HashFunc = sha1Hash): 
     truncated = truncated mod uint64(10 ^ digits)
     result = align($truncated, digits, '0')
 
-proc totp*(key: Bytes, digits = 6, interval: int64 = 30, hash: HashFunc = sha1Hash, now: int64 = (int64)(epochTime()), t0: int64 = 0): string =
+proc totp*(key: Bytes, digits: OtpValueLen = 6, interval: TimeInterval = 30,
+           hash: HashFunc = sha1Hash, now: EpochSecond = (EpochSecond)(epochTime()), t0: EpochSecond = 0): string =
     ## Generates TOTP value from `key` using `t0` as the initial point in time
     ## to begin counting the time steps and the interval of each time step is
     ## 30 seconds by default. `t0` is Unix epoch so it is set to 0 by default.
-    let c = (now - t0) div interval
+    let c = (now - t0) div (EpochSecond)(interval)
     result = hotp(key, c.uint64, digits, hash)
 
 proc b32totp*(key: string, digits = 6, inteval: int64 = 30): string =
     ## Google Authenticator compatible TOTP where the 'key' is given as a
     ## Base-32 encoded string.
     result = totp(key.base32Decode)
+
+proc at*(hotp: Hotp, counter: BiggestUInt): string =
+    result = hotp(hotp.key, counter, hotp.length, hotp.hashFunc)
+
+proc at*(totp: Totp, now: EpochSecond = (uint64)(epochTime())): string =
+    result = totp(totp.key, totp.length, totp.interval, totp.hashFunc, now, totp.t0)
